676c9a0bf84141f4aba8ea042f377b28bf6b2dd1
popow <bassam@naska.id>
2025-10-06 18:50:24 +0700

    fix repo

--- internal/service/update_attendance.go
+++ internal/service/update_attendance.go
@@ -3,7 +3,7 @@ package service
 import (
 	"context"
 
-	"github.com/fofow/backend-go/model"
+	"github.com/fofow/backend-go/internal/model"
 )
 
 func (s *service) UpdateAttendance(ctx context.Context, uuid string) (res model.GetDataSinarmasResponse, err error) {
